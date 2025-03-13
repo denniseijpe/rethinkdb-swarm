69a76c029747d99c92a93432659383eda3ef39ad
Dennis Eijpe <dennis@de-programmeer.nl>
2025-03-13 13:42:02 +0100

    Force to rethinkdb 2.4.4

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM rethinkdb:2
+FROM rethinkdb:2.4.4-bookworm-slim
 
-ADD entrypoint.sh /bin/
-ADD rethinkdb-probe /bin/
+COPY entrypoint.sh /bin/
+COPY rethinkdb-probe /bin/
 
 HEALTHCHECK --interval=20s --timeout=10s --retries=3 \
     CMD /bin/rethinkdb-probe
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,21 @@ shopt -s nullglob
 
 # if command starts with an option, prepend rethinkdb
 if [ "${1:0:1}" = '-' ]; then
-	set -- rethinkdb $@
+	set -- rethinkdb --no-update-check $@
 fi
 
 if [ "$1" = 'admin' ]; then
   # Check whether a server is online
   getent hosts tasks.rethinkdb_server || (echo "[Error] No servers found!" && exit 1)
 
-  set -- rethinkdb proxy --bind all --initial-password $RETHINKDB_PASSWORD --join tasks.rethinkdb_server
+  set -- rethinkdb proxy --bind all --no-update-check --initial-password $RETHINKDB_PASSWORD --join tasks.rethinkdb_server
 fi
 
 if [ "$1" = 'default' ]; then
-	set -- rethinkdb --bind all --no-http-admin --initial-password $RETHINKDB_PASSWORD
+	set -- rethinkdb --bind all --no-update-check --no-http-admin --initial-password $RETHINKDB_PASSWORD
 
 	# Check whether there are other servers to join
-	getent hosts tasks.rethinkdb_server && set -- rethinkdb --bind all --no-http-admin --initial-password $RETHINKDB_PASSWORD --join tasks.rethinkdb_server
+	getent hosts tasks.rethinkdb_server && set -- rethinkdb --bind all --no-update-check --no-http-admin --initial-password $RETHINKDB_PASSWORD --join tasks.rethinkdb_server
 fi
 
 exec "$@"
